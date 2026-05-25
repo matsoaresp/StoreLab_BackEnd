4d5550b3cb638ebe45764119236a5984646365f5
Mateus Prado <maateussp@gmail.com>
2026-05-25 09:47:04 -0300

    fix: correcting project entities

---
+++ appstore/src/test/java/com/unifil/cassino/CassinoApplicationTests.java
@@ -0,0 +1,12 @@
+package test.java.com.unifil.cassino;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CassinoApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
