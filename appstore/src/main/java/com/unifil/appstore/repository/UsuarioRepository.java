fb824a15bc0ff3249a4a243d794b8ff83a535b39
Mateus Prado <maateussp@gmail.com>
2026-05-28 10:49:44 -0300

    feat: configurando a seguranca da aplicacao

--- appstore/src/main/java/com/unifil/appstore/repository/UsuarioRepository.java
+++ appstore/src/main/java/com/unifil/appstore/repository/UsuarioRepository.java
@@ -1,8 +1,13 @@
 package com.unifil.appstore.repository;
+
 import com.unifil.appstore.models.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+
 }
